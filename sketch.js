776c144fdff1cd3d8bb533d54e147e61e060bb44
Taylor Butler <TaylorButler@Lauras-MacBook-Pro-2.local>
2019-09-03 12:33:22 -0500

    stuff

--- sketch.js
+++ sketch.js
@@ -1,7 +1,78 @@
 function setup() {
-    createCanvas(800, 600);
-    background(0);
-    ellipse(2,2,20,50,);
+    createCanvas(800,600);
+    background(255,255,255);
+
+    fill(0,0,0);
+    noStroke();
+    ellipse(400,300,100,100);
+
+        fill(0,0,0);
+        strokeWeight(1);
+        stroke(0);
+        ellipse(400,300,110,110);
+        
+        fill(0,0,0,0);
+        strokeWeight(1);
+        stroke(0);
+        ellipse(400,300,160,160);
+
+        fill(0,0,0,0);
+        strokeWeight(1);
+        stroke(0);
+        ellipse(400,300,210,210);
+
+        fill(0,0,0,0);
+        strokeWeight(1);
+        stroke(0);
+        ellipse(400,300,260,260);
+
+        fill(0,0,0,0);
+        strokeWeight(1);
+        stroke(0);
+        ellipse(400,300,310,310);
+
+        fill(0,0,0,0);
+        strokeWeight(1);
+        stroke(0);
+        ellipse(400,300,460,460);
+
+        fill(0,0,0,0);
+        strokeWeight(1);
+        stroke(0);
+        ellipse(400,300,510,510);
+
+        fill(0,0,0,0);
+        strokeWeight(1);
+        stroke(0);
+        ellipse(400,300,660,660);
+
+        fill(0,0,0,0);
+        strokeWeight(1);
+        stroke(0);
+        ellipse(400,300,710,710);
+
+        fill(0,0,0,0);
+        strokeWeight(1);
+        stroke(0);
+        ellipse(400,300,860,860);
+
+    fill(0,0,0);
+    noStroke();
+    ellipse(400, 810, 2000, 1000);
+
+        fill(0,0,0);
+        strokeWeight(1);
+        stroke(255,255,255);
+        ellipse(400,810,2000,1000);
+
+        ellipse(400,818,2000,1000);
+        ellipse(400,830,2000,1000);
+        ellipse(400,845,2000,1000);
+        ellipse(400,870,2000,1000);
+        ellipse(400,900,2000,1000);
+        ellipse(400,940,2000,1000);
+        ellipse(400,980,2000,1000);
+        ellipse(400,1050,2000,1000);
 }
 
 function draw() {
